0e8d664b87a10b2a5706f4ef50e7ab872a69ec48
Anil Thapa <anilthapa1.at23@gmail.com>
2024-09-15 21:00:00 -0500

    Added Filter and Map functions

---
+++ Dynamic Programming/8_all_construct.py
@@ -0,0 +1,22 @@
+def bestSum(target, arr, memo = []):
+    if target == 0: return []
+    if target < 0: return -1
+
+    shortestLength = None
+
+    for num in arr:
+        remainder = target - num
+        remainder_result = bestSum(remainder, arr, memo)
+        if remainder_result != -1:
+            memo.append(num)
+            if len(memo) < len(shortestLength):
+                pass
+
+
+
+    return  -1
+
+print(bestSum(7, [5, 3, 4, 7]))
+print(bestSum(8, [5, 3, 2]))
+print(bestSum(8, [1, 4, 5]))
+print(bestSum(100, [1, 2, 5, 25]))
