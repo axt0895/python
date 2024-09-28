ed17488bf84aaaeb9c22739d0ede2f3f601045e2
Anil Thapa <anilthapa1.at23@gmail.com>
2024-09-27 21:00:00 -0500

    Updated bubbble sort algorithm

--- Sorting Algorithm/2.bubble_sort.py
+++ Sorting Algorithm/2.bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(arr):
     for i in range(len(arr)):
         swapped = False
-        for j in range(len(arr)-i):
+        for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
