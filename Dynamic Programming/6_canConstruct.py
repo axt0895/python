302e8fac274553b5c3fb02488c914c7e56e4ae6c
= <=>
2024-10-07 20:36:02 -0500

    Added Filter and Map functions

--- Dynamic Programming/6_canConstruct.py
+++ Dynamic Programming/6_canConstruct.py
@@ -1,19 +1,19 @@
-def canConstruct(word, arr):
-    if word is None: return True
+def can_construct(target, arr, memo = {}):
+    if target in memo: return  memo[target]
+    if target == '': return True
+    for word in arr:
+        if target.startswith(word):
+            new_target = target[len(word):]
+            if can_construct(new_target, arr, memo):
+                memo[target] = True
+                return True
 
-    for a in arr:
-       if word.index(a) == 0:
-           suffix = word[len(a):]
-           if canConstruct(suffix, arr):
-               return True
-           
-           
+    memo[target] = False
     return False
 
 
 
-print(canConstruct('Anil', ['A', 'nil']))
-print(canConstruct('Watermelon', ['water', 'n']))
-           
-           
-           
+print(can_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'adcd']))
+print(can_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
+print(can_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
+print(can_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eeee', 'eeeeee', 'eeeeeee', 'eeeeeeee']))
