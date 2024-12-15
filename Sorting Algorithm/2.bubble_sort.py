fdf6bd32585b68da309542a83345e9a18f2160c3
= <=>
2024-12-15 01:38:28 -0600

    Modified bubble sort algorithm

--- Sorting Algorithm/2.bubble_sort.py
+++ Sorting Algorithm/2.bubble_sort.py
@@ -1,10 +1,22 @@
+import random
+
+def generate_random_array(n):
+    arr = []
+    for i in range(n):
+        a = random.randint(0, n * 10)
+        arr.append(a)
+    return arr
+
 def bubble_sort(arr):
     for i in range(len(arr)):
-        swapped = False
         for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        if not swapped:
-            break
     return arr
+
+
+if __name__ == "__main__":
+    arr = generate_random_array(14)
+    print(arr)
+    arr1 = bubble_sort(arr)
+    print(arr1)
