c9d7074c0bd469c1e38bfee5e0df34d969967c3c
Anil Thapa <anilthapa1.at23@gmail.com>
2025-01-08 19:02:06 -0600

    Completed Tracker portion for Players and Football

--- Football Analysis/main.py
+++ Football Analysis/main.py
@@ -6,9 +6,9 @@ def main():
     video_frames = read_video('/Users/anilthapa/python/Football Analysis/input_videos/premier_league_30.mp4')
     
     # Initialize the Tracker
-    tracker = Tracker('/Users/anilthapa/python/Football Analysis/output_videos/detect.avi')
+    tracker = Tracker('/Users/anilthapa/Downloads/best.pt')
     
-    tracks = tracker.get_object_track(video_frames)
+    tracks = tracker.get_object_track(video_frames, read_from_stub = True, stub_path = '/Users/anilthapa/python/Football Analysis/stubs/track_stubs.pkl')
     
     # Save video
     save_video(video_frames, '/Users/anilthapa/python/Football Analysis/output_videos/detect.avi')
--- Football Analysis/trackers/trackers.py
+++ Football Analysis/trackers/trackers.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
-
+import pickle
+import os
 
 
 class Tracker:
@@ -10,27 +11,58 @@ class Tracker:
         self.tracker = sv.ByteTrack()
     
     def detect_frames(self, frames):
-        batch_size = 20
+        batch_size = 32
         detections = []
         
         for i in range(0, len(frames), batch_size):
             detection_batch = self.model.predict(frames[i:i+batch_size], conf = 0.2)
             detections += detection_batch
         return detections
+
+    def get_object_track(self, frames, read_from_stub=False, stub_path = None):
         
-    def get_object_tracker(self, frames):
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as file:
+                tracks = pickle.load(file)
+            return tracks
+            
         detections = self.detect_frames(frames)
+        tracks = {
+            "Football": [],
+            "Player": [],
+            "refere":[]
+        }
         
         for frame_num, detection in enumerate(detections):
+            class_names = detection[0].names
+            class_names_inv = {v:k for k, v in class_names.items()}
             
-            # Access class names from the model
-            class_names = self.model.names
-            class_name_inv = {v:k for k, v in class_names.items()}
-            
-            # Convert YOLO detection to supervision format
+            #  Convert to supervision detection format
             detection_supervision = sv.Detections.from_ultralytics(detection)
             
-            # Track objects using ByteTrack
-            tracked_objects = self.tracker.update_with_detections(detection_supervision)
+            # Track Objects
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
             
-        
+            tracks["Football"].append({})
+            tracks["Player"].append({})
+            tracks["refere"].append({})
+            
+            for frame_detection in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                class_id = frame_detection[3]
+                tracker_id = frame_detection[4]
+                
+                if class_id == class_names_inv['Player']:
+                    tracks['Player'][frame_num][tracker_id] = {'bbox': bbox}
+                if class_id == class_names_inv['refere']:
+                    tracks['refere'][frame_num][tracker_id] = {'bbox': bbox}
+            
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                class_id = frame_detection[3]
+                if class_id == class_names_inv['Football']:
+                    tracks['Football'][frame_num][1] = {'bbox': bbox}
+        if stub_path is not None:
+            with open(stub_path, 'wb') as file:
+                pickle.dump(tracks, file)
+        return tracks
